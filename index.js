a3ac37761da302e38aa307c2ad43422163b58582
rshammad <63588814+rshammad@users.noreply.github.com>
2024-09-22 14:59:32 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node! Production site used for this lab\n'
+  const msg = 'Hello Node! Staging site used for this lab\n'
   res.end(msg);
 });
 
